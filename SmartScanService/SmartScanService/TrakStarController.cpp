a58bb1ce6932550ce9661f29d2ba1d50fe7b27e5
Eduard Cazacu <eduard.cazacu@sorama.eu>
2020-12-01 11:02:56 +0100

    Moved the dirvers and copied some code to TrakStarController class

--- SmartScanService/SmartScanService/TrakStarController.cpp
+++ SmartScanService/SmartScanService/TrakStarController.cpp
@@ -1,3 +1,17 @@
 #include "TrakStarController.h"
 
 using namespace SmartScan;
+
+//constructor
+TrakStarController::TrakStarController()
+{
+	
+}
+//init func
+void TrakStarController::Init()
+{
+	printf("Initializing ATC3DG system...\n");
+	errorCode = InitializeBIRDSystem();
+	if (errorCode != BIRD_ERROR_SUCCESS) errorHandler(errorCode);
+
+}
--- SmartScanService/SmartScanService/TrakStarController.h
+++ SmartScanService/SmartScanService/TrakStarController.h
@@ -3,12 +3,50 @@
 //functionality such as correct configuration, starting, stopping the scan and
 //getting new data is handled here.
 
+#include "NDI/ATC3DG.h"				//TrakSTAR driver
+#include <time.h>
+
 namespace SmartScan
 {
+	class CSystem
+	{
+	public:
+		SYSTEM_CONFIGURATION	m_config;
+	};
+
+	class CSensor
+	{
+	public:
+		SENSOR_CONFIGURATION	m_config;
+	};
+
+	class CXmtr
+	{
+	public:
+		TRANSMITTER_CONFIGURATION	m_config;
+	};
+
+
 	class TrakStarController
 	{
 	public:
+		TrakStarController();
+
+
+		void Init();
 
 	private:
+		CSystem			ATC3DG;
+		CSensor* pSensor;
+		CXmtr* pXmtr;
+		int				errorCode;
+		int				i;
+		int				sensorID;
+		short			id;
+		int				records = 100;
+		char			output[256];
+		int				numberBytes;
+		clock_t			goal;
+		clock_t			wait = 10;	// 10 ms delay
 	};
 }
