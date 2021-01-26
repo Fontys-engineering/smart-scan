7b91275aeda089e8eb47528724bc1dfb1362a5fc
coenvdgrinten <78020827+coenvdgrinten@users.noreply.github.com>
2021-01-26 12:44:17 +0100

    Added all headers

--- SmartScanService/SmartScanService/Filtering.cpp
+++ SmartScanService/SmartScanService/Filtering.cpp
@@ -1,3 +1,25 @@
 #include "Filtering.h"
 
 using namespace SmartScan;
+
+//Filtering helper methods:
+void RotationOrientation(std::vector<Point3>& data, std::vector<Point3>& referenceData);
+
+void FilterIteration(std::vector<Point3>& data, std::vector<ReferencePoint> referencePoints, double resolution);
+
+void CalculateCoordinates(ReferencePoint ref, std::vector<Point3>& data);
+
+void Outlier(std::vector<Point3>& data, double phi_range, double theta_range);
+
+/* <summary
+* Calculate the arc tangent of two points. Output = arctan(b/a)
+* Takes into account the four quadrants of the function, so range is from -180 to 180 degrees
+* </summary>
+*/
+double arctan(double a, double b);
+
+bool TestPoint(std::vector<Point3>& data, double phi_range, double theta_range);
+
+std::vector<Point3>& GradientSmoothing(std::vector<Point3>& data, double phi_range, double theta_range);
+
+std::vector<std::vector<Point3>>& SortArrays(std::vector<Point3>& data, std::vector<Point3> reference_data);
