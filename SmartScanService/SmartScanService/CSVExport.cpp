864a99016ee0c79efc25ac0f619dfaa9a60f2ec0
Jesper van Valburg <j.vanvalburg@student.fontys.nl>
2021-04-13 12:07:06 +0200

    Export sensor data in seperate columns

--- SmartScanService/SmartScanService/CSVExport.cpp
+++ SmartScanService/SmartScanService/CSVExport.cpp
@@ -1,5 +1,5 @@
 #include "CSVExport.h"
-
+#include <iostream>
 #include <chrono>
 
 using namespace SmartScan;
@@ -17,9 +17,13 @@ void CSVExport::ExportPoint3(const std::vector<Point3>& data, const std::string 
 	csvFile << data.size() << "," << NUsedSensors << std::endl;    //Add info on the first line
 	if (data.size() > 0)    //Write to file:
 	{
-		for (auto& record : data)    //Access by reference to avoid copying
+		for (int i = 0; i < data.size(); i = i+NUsedSensors)    //Access by reference to avoid copying
 		{
-			csvFile << record.time << "," << record.x << "," << record.y << "," << record.z << "," << record.r.x << "," << record.r.y << "," << record.r.z << "," << record.quality << "," << (int)record.buttonState << std::endl;
+			for (int j = 0; j < NUsedSensors; j++)
+			{
+				csvFile << data.at(i + j).time << "," << data.at(i + j).x << "," << data.at(i + j).y << "," << data.at(i + j).z << "," << data.at(i + j).r.x << "," << data.at(i + j).r.y << "," << data.at(i + j).r.z << "," << data.at(i + j).quality << "," << (int)data.at(i + j).buttonState << ",";
+			}
+			csvFile << std::endl;
 		}
 	}
 	csvFile.close();
